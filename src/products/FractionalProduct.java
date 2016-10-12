141af1e07e8e5bd0730dc01ba78a8d39d7e8d5e0
Martin Montenegro <martin.92m@gmail.com>
2016-10-11 23:03:10 -0300

    Add LiquidProduct

--- src/products/FractionalProduct.java
+++ src/products/FractionalProduct.java
@@ -6,14 +6,11 @@ import java.math.BigDecimal;
  * Created by montenegromi on 11/10/16.
  */
 public class FractionalProduct extends Product {
+    // Could be modeled
     private String unit;
 
     public FractionalProduct(String name, BigDecimal price, String unit) {
         super(name, price);
-        this.unit = unit;
-    }
-
-    public void setUnit(String unit) {
         this.unit = unit;
     }
 
